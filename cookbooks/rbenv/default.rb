2b157e24c5f65a8a330bd425f58ffcfeeb45f121
shiroemons <shiroemons@gmail.com>
2019-07-22 20:40:15 +0900

    .bashrcにrbenvの読み込み処理を追加

--- cookbooks/rbenv/default.rb
+++ cookbooks/rbenv/default.rb
@@ -1,1 +1,15 @@
 include_recipe "rbenv::system"
+
+file '/home/shiroemons/.bashrc' do
+  action :edit
+  block do |content|
+    next if content.match %r{export RBENV_ROOT=/usr/local/rbenv}
+    appendix = <<~EOS
+      # rbenv
+      export RBENV_ROOT=/usr/local/rbenv
+      export PATH="${RBENV_ROOT}/bin:${PATH}"
+      eval "$(rbenv init -)"
+    EOS
+    content << appendix
+  end
+end
